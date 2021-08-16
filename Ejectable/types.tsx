e6b3ee2ea6558eb0be3e14051771c313852e3993
Ren Juan <jdaugherty@anyjuan.meansoforproduction.biz>
2021-08-16 07:50:50 +0000

    *

---
+++ Ejectable/types.tsx
@@ -0,0 +1,22 @@
+/**
+ * Learn more about using TypeScript with React Navigation:
+ * https://reactnavigation.org/docs/typescript/
+ */
+
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Trip: undefined;
+  Settings: undefined;
+};
+
+export type TripParamList = {
+  TripScreen: undefined;
+};
+
+export type SettingsParamList = {
+  SettingsScreen: undefined;
+};
