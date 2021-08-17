833247112aacf903f3862477b8d128d41609aca2
Ren Juan <juan@acm.org>
2021-08-17 19:34:08 +0000

    *

--- Ejectable/components/ScreenInfo.tsx
+++ Ejectable/components/ScreenInfo.tsx
@@ -6,15 +6,26 @@ import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
-export default function SettingsScreenInfo() {
+export default function ScreenInfo() {
   return (
     <View style={styles.settingsContainer}>
         <Text
           style={styles.settingsText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Toggle km/mi, set CO2/ea
-                   
+          Toggle km/mi, select fuel used, or manually set CO2 per distance                  
+        </Text>  
+    </View>
+  );
+}
+export function ScreenInfo2() {
+  return (
+    <View style={styles.settingsContainer}>
+        <Text
+          style={styles.settingsText}
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Use Pause if fuel burning portion of trip interrupted                  
         </Text>  
     </View>
   );
@@ -23,6 +34,7 @@ export default function SettingsScreenInfo() {
 const styles = StyleSheet.create({
   settingsContainer: {
     marginHorizontal: 50,
+    marginVertical: 20,
   },
   settingsText: {
     fontSize: 17,
